64f3c8b299422f6e0a2a78209fae19f69e167e1a
timfiloye <timfiloye69@gmail.com>
2024-05-20 13:34:42 -0700

    login and registration screen added

--- frontEnd/school-app/src/App.js
+++ frontEnd/school-app/src/App.js
@@ -1,27 +1,11 @@
 import './App.css';
-import React, { useEffect } from 'react'
+import LoginRegister from './Components/LoginRegister/LoginRegister';
 
 function App() {
-  const [data, setData] = React.useState('')
-  const getData = async () => {
-    const res = await fetch('/users')
-    const data = await res.json();
-    setData(data.data)
-    console.log(data.data);
-  }
-
-  useEffect(() => {
-    getData()
-  }, [])
-  
-
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>{data.name}</h1>
-        <p>{data.age}</p>
-        <h2>{data.position}</h2>
-      </header>
+     <LoginRegister/>
     </div>
   );
 }
